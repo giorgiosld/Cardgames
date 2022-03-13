bafd369c6702980f07e695291d7ae8f9a3b571f5
girogiosld <giorgiosaldana99@gmail.com>
2022-03-13 22:28:48 +0100

    Last commit

--- app/src/main/java/it/unicam/cs/pa2021/cardgames/App.java
+++ app/src/main/java/it/unicam/cs/pa2021/cardgames/App.java
@@ -42,6 +42,7 @@ public class App {
         }
         for(BlackJackIPlayer bjp : bjTable.getPlayers())
             System.out.println("Partita terminata con "+bjp.getNome()+" avente "+bjp.getBank()+" coin");
+
     }
 }
 
